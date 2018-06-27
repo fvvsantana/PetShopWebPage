b75064d378928925f830547ca77fb28e4cac6376
Unknown <mateus.kastilho@gmail.com>
2018-06-27 19:39:32 -0300

    Create a Server for the SPA

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
-var express = require('express'),
-    app = express();
-    
-console.log('oi');
+let express = require('express');
+let app = express();
+app.use(express.static(__dirname + '/client'));
+
+app.get('/', (req, res)=>{
+   res.sendFile('/client/index.html', {root: __dirname });
+})
+
+let server = app.listen(3000, ()=>{
+   let host = server.address().address;
+   let port = server.address().port;
+   console.log("Example app listening at http://%s:%s", host, port);
+})

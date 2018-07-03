87d8c57628e21d4471f6dfdaa71b50e40a3d72af
César Lima <cesaralbc@gmail.com>
2018-07-03 15:10:26 -0300

    Restore server port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get('/', (req, res)=>{
    res.sendFile('/client/index.html', {root: __dirname });
 })
 
-let server = app.listen(80, ()=>{
+let server = app.listen(3000, ()=>{
    let host = server.address().address;
    let port = server.address().port;
    console.log("Example app listening at http://%s:%s", host, port);
